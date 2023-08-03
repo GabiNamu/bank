b9593078186b9c7356abb28d537ef7143f18106c
Gabriela Muniz <gabrielanamu@live.com>
2023-08-03 10:02:11 -0300

    feat: add models

---
+++ backend/src/database/models/Account.model.ts
@@ -0,0 +1,48 @@
+import { Model, INTEGER, STRING, BOOLEAN, DataTypes } from 'sequelize';
+import db from '.';
+import IAccount from '../../interfaces/IAccount';
+
+class Account extends Model<IAccount> {
+	declare 'id': number;
+	declare 'cpf/cnpj': string;
+	declare 'name': string;
+	declare 'email': string;
+	declare 'password': string;
+	declare 'status': boolean;
+}
+
+Account.init({
+	id: {
+		allowNull: false,
+		autoIncrement: true,
+		primaryKey: true,
+		type: INTEGER,
+	},
+	'cpf/cnpj': {
+		allowNull: false,
+		type: STRING,
+	},
+	name: {
+		allowNull: false,
+		type: STRING,
+	},
+	email: {
+		allowNull: false,
+		type: DataTypes.STRING,
+	},
+	password: {
+		allowNull: false,
+		type: STRING,
+	},
+	status: {
+		allowNull: false,
+		type: BOOLEAN,
+	}
+}, {
+	sequelize: db,
+	modelName: 'accounts',
+	timestamps: false,
+	underscored: true,
+});
+
+export default Account;
---
+++ backend/src/database/models/Transaction.model.ts
@@ -0,0 +1,55 @@
+import { Model, INTEGER, DATEONLY, DECIMAL } from 'sequelize';
+import db from '.';
+import ITransaction from '../../interfaces/ITransaction';
+import Account from './account.model';
+
+class Transaction extends Model<ITransaction> {
+	declare 'id': number;
+	declare 'price': number;
+	declare 'accountId': number;
+	declare 'date': Date;
+	declare 'cashback'?: number;
+}
+
+Transaction.init({
+	id: {
+		allowNull: false,
+		autoIncrement: true,
+		primaryKey: true,
+		type: INTEGER,
+	},
+	price: {
+		allowNull: false,
+		type: DECIMAL(10, 2),
+	},
+	accountId: {
+		allowNull: false,
+		field: 'account_id',
+		type: INTEGER,
+		onUpdate: 'CASCADE',
+		onDelete: 'CASCADE',
+		references: {
+			model: 'accounts',
+			key: 'id',
+		}
+	},
+	date: {
+		allowNull: false,
+		type: DATEONLY,
+		defaultValue: new Date(),
+	},
+	cashback: {
+		allowNull: true,
+		type: INTEGER,
+	},
+}, {
+	sequelize: db,
+	modelName: 'transactions',
+	timestamps: false,
+	underscored: true,
+});
+
+Transaction.belongsTo(Account, { foreignKey: 'accountId', as: 'account' });
+Account.hasMany(Transaction, { foreignKey: 'accountId', as: 'transaction' });
+
+export default Transaction;
